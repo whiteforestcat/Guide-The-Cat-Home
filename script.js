6e481fc2760fa148cdb3c6915a34e9e315b111ca
Muhammad Amir Mohamad Kamal <whiteforestcat96@gmail.com>
2022-12-19 10:56:23 +0800

    create player

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ canvas.width = window.innerWidth; // to make canvas take the full width of the s
 canvas.height = window.innerHeight; // to make canvas take the full height of the screen
 
 console.log(c);
+
+// to create player
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 100, // 100 units to the right
+      y: 100, // 100 units downwards
+    };
+    this.width = 30; // charcter size
+    this.height = 30;
+    this.velocity = {
+      x: 0,
+      y: 1, // x-y velocity coordinate, y = 1 cos want gravity to bring it down
+    };
+  }
+}
