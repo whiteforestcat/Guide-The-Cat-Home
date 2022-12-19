c8880e6746041b5d871310aad3a08649002d92b5
Muhammad Amir Mohamad Kamal <whiteforestcat96@gmail.com>
2022-12-19 10:58:26 +0800

    drawing the character and introducing velocity

--- script.js
+++ script.js
@@ -24,4 +24,26 @@ class Player {
       y: 1, // x-y velocity coordinate, y = 1 cos want gravity to bring it down
     };
   }
+
+  draw() {
+    // function within class
+    c.fillStyle = "red"; // make character red, need to style first before creating the character using .fillRect
+    // to draw the player
+    c.fillRect(this.position.x, this.position.y, this.width, this.height); // input parameters as the coordinates of the player
+  }
+
+  update() {
+    this.position.y += this.velocity.y; // need to update y-coordinates first before draw() to work
+    this.position.x += this.velocity.x; // why need this for keydown right to work? this is called by animate() which will keep iterating
+    this.draw();
+    // this.velocity.y += gravity; // to create acceleration along y axis
+
+    // to stop player from dropping below the screen
+    if (this.position.y + this.height <= canvas.height) {
+      // need to plus this.height so that object will stop falling one "object-height" away from canvas bottom
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+  }
 }
